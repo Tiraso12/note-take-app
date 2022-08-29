dcae1b904c6a61baf3d7312a58491f483fb83743
Gonzalo <gonzalo14_2@hotmail.com>
2022-08-29 13:18:18 -0400

    trying to render html

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
+app.set('public', path.join(__dirname, 'public'));
+
+app.get('/public/index.html', (req, res) =>{
+  res.render('index.html');
+})
+
 
 app.listen(3001,()=>{
   console.log("PORT ONLINE");
 });
-
-app.get('/', (req, res) =>{
-  res.status(500).send("Hi");
-})
