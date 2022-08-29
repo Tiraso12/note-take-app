0a26f8c27e2faed05775c087492bf089ff8cedbe
Gonzalo <gonzalo14_2@hotmail.com>
2022-08-29 13:07:09 -0400

    reorganizing folder structure

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-  });
+app.listen(3001,()=>{
+  console.log("PORT ONLINE");
+});
+
+app.get('/', (req, res) =>{
+  res.status(500).send("Hi");
+})
