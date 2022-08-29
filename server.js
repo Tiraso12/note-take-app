14805c9654d61a253837a688808ca90cda35aae9
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2022-08-29 14:06:33 -0400

    apis for html created

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
+const data = require('./db/db.json');
+
+
+
 const app = express();
 
-app.listen(3001,()=>{
-  console.log("PORT ONLINE");
-});
-
-app.get('/', (req, res) =>{
-  res.status(500).send("Hi");
+// api to display html file
+app.get('/html', (req, res) =>{
+ 
+  res.sendFile(__dirname + '/public/index.html');
+  
 })
+
+//api to notes
+app.get('/notes', (req, res)=>{
+
+  res.sendFile(__dirname + '/public/notes.html');
+})
+
+//api to db.json
+app.get('/api/data', (req, res)=>{
+  res.json(data);
+})
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
